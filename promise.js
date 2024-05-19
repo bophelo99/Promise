06e7fd9f8d56c8de736f60829fdfefe972992a4a
Bophilo Ntsemi <ntsemibophilo@gmail.com>
2024-05-19 11:16:05 +0200

    Async

--- promise.js
+++ promise.js
@@ -1,15 +1,35 @@
+
+/*
+In the following code we try to get list of all entries from remote url and then based on that make request about each of the 
+category. Finally print them all out. We are using axios get, which returns a promise. 
+*/
 const axios = require('axios');
-const connectToURL = async (url) => {
-    try {
-        const response = await axios.get(url);
-        const listOfEntries = response.data.entries;
-        listOfEntries.forEach((entry) => {
-            console.log(entry.Category);
+/*
+In the following code we try to get list of all entries from remote url and then based on that make request about 
+each of the category. Finally print them all out. We are using axios get, which returns a promise. 
+*/
+const connectToURL = (url)=>{
+  const req = axios.get(url);
+  req.then(resp => {
+      let listOfEntries = resp.data.entries;
+      return listOfEntries.map((entry)=>{
+          return entry.Category
+      })
+    }).then((categories)=>{
+      let Categories = categories.filter(function(item, pos, self) {
+        return self.indexOf(item) == pos;
+    })
+        Categories.forEach((category)=>{
+            const req = axios.get("https://api.publicapis.org/entries?Category="+category);
+            req.then(resp=>{
+                console.log(category+" - "+resp.data.count);
+            }).catch(err => {
+              
+            })
         });
-    } catch (error) {
-        console.error(error.toString());
-    }
+    })
+  .catch(err => {
+      console.log(err.toString())
+  });
 }
-console.log("Before connect URL");
 connectToURL('https://api.publicapis.org/entries');
-console.log("After connect URL");
