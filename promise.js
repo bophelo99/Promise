11103bdc5fb74dbf6f3ef90cd8e8f650283b3c94
Bophilo Ntsemi <ntsemibophilo@gmail.com>
2024-05-19 11:17:17 +0200

    Async

--- promise.js
+++ promise.js
@@ -1,35 +1,31 @@
-
 /*
 In the following code we try to get list of all entries from remote url and then based on that make request about each of the 
-category. Finally print them all out. We are using axios get, which returns a promise. 
+category starting with 'A'. Finally print the API counts of the category. We are using axios get, which returns a promise. 
 */
 const axios = require('axios');
-/*
-In the following code we try to get list of all entries from remote url and then based on that make request about 
-each of the category. Finally print them all out. We are using axios get, which returns a promise. 
-*/
-const connectToURL = (url)=>{
-  const req = axios.get(url);
-  req.then(resp => {
-      let listOfEntries = resp.data.entries;
-      return listOfEntries.map((entry)=>{
+async function connectToURL(url){
+    const resp = await axios.get(url);
+    let listOfEntries = resp.data.entries;
+    let Categories = listOfEntries.map((entry)=>{
           return entry.Category
-      })
-    }).then((categories)=>{
-      let Categories = categories.filter(function(item, pos, self) {
-        return self.indexOf(item) == pos;
-    })
-        Categories.forEach((category)=>{
-            const req = axios.get("https://api.publicapis.org/entries?Category="+category);
-            req.then(resp=>{
-                console.log(category+" - "+resp.data.count);
-            }).catch(err => {
-              
-            })
-        });
-    })
-  .catch(err => {
-      console.log(err.toString())
-  });
+    });
+    Categories = [...new Set(Categories)];
+    Categories.forEach(async (Category)=>{
+      if (Category.startsWith("A")) {
+              try {
+                const resp = await axios({
+                  method: 'get',
+                  url: "https://api.publicapis.org/entries?Category="+Category,
+                  responseType: 'json'
+                })
+                console.log(Category+"   "+resp.data.count);
+              } 
+              catch(e) {
+                console.log(e);
+              }
+      }
+    });
 }
-connectToURL('https://api.publicapis.org/entries');
+connectToURL('https://api.publicapis.org/entries').catch(err => {
+    console.log(err.toString())
+});
